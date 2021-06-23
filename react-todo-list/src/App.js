2b3c2d96bbef15e810c53e527c3583e9a7d81911
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2021-06-23 18:01:37 +0800

    .

---
+++ react-todo-list/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, {useState} from "react";
+import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
+
+function App() {
+  const [todos,setTodos] = useState([])
+
+  function addTodo(todo){
+    setTodos([todo,...todos])
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Todo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
